494d8aa39b8ba414186db449244ee2e2d8d8b22b
Abhai Sasidharan <abhaisasidharan@gmail.com>
2021-04-02 13:41:37 +0530

    jQuery works too

--- js/nephos.ts
+++ js/nephos.ts
@@ -4,11 +4,13 @@
 
 declare var Drupal: any;
 declare var jQuery: any;
+let $ = jQuery;
 import '../scss/nephos.scss';
 import './if-you-want-to-write-js-code';
 
 Drupal.behaviors.nephos = {
   attach: function () {
-    console.log('TypeScript here!');
+    console.log('TypeScript here! Jquery Works too.');
+    console.log($('body').attr('class'));
   }
 }
